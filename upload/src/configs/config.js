51419b88f908899f7244e63b03a3a07aca96b2ee
tuan-hda <anhtuan.hoangdinh@gmail.com>
2023-11-29 16:17:02 +0700

    add upload api

--- upload/src/configs/config.js
+++ upload/src/configs/config.js
@@ -1,18 +1,11 @@
 const dotenv = require('dotenv')
 
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config({
-    path: '.env.dev',
-  })
-} else {
-  dotenv.config()
-}
+dotenv.config()
 
 module.exports = {
   PORT: process.env.PORT || 8002,
   JWT_SECRET: process.env.JWT_SECRET,
   whitelist: ['http://localhost:3000'],
-  DOMAIN_NAME: process.env.DOMAIN_NAME || 'http://localhost:8002/',
   AWS_CLOUDFRONT_DOMAIN: process.env.AWS_CLOUDFRONT_DOMAIN || 'https://d1vh9hdowc7dho.cloudfront.net/',
   AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME || 'devorum',
   AWS_BUCKET_REGION: process.env.AWS_BUCKET_REGION || 'ap-southeast-1',
--- upload/src/index.js
+++ upload/src/index.js
@@ -10,6 +10,7 @@ const { Upload } = require('@aws-sdk/lib-storage')
 const { S3Client } = require('@aws-sdk/client-s3')
 const fs = require('fs')
 const createHttpError = require('http-errors')
+const { v4: uuidv4 } = require('uuid')
 
 app.use(cors({ origin: config.whitelist, credentials: true }))
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -20,41 +21,53 @@ const s3Client = new S3Client({
   credentials: { accessKeyId: config.AWS_ACCESS_KEY, secretAccessKey: config.AWS_SECRET_KEY },
 })
 
-app.post('/', upload.single('file'), async (req, res, next) => {
-  try {
-    if (req.file == null) {
-      return createHttpError[400]('You must choose a file')
+app.post(
+  '/',
+  (_, __, next) => {
+    try {
+      fs.mkdirSync('uploads')
+    } catch (error) {
+      if (error.code !== 'EEXIST') {
+        console.log(error)
+      }
     }
-    var file = req.file
+    next()
+  },
+  upload.single('file'),
+  async (req, res, next) => {
+    try {
+      if (req.file == null) {
+        return createHttpError[400]('You must choose a file')
+      }
+      var file = req.file
 
-    const target = {
-      Bucket: config.AWS_BUCKET_NAME,
-      region: config.DOMAIN_NAME,
-      Key: req.file.originalname,
-      Body: fs.createReadStream(file.path),
+      const target = {
+        Bucket: config.AWS_BUCKET_NAME,
+        region: config.DOMAIN_NAME,
+        Key: uuidv4() + '-' + req.file.originalname,
+        Body: fs.createReadStream(file.path),
+      }
+      const parallelUploads3 = new Upload({
+        client: s3Client,
+        queueSize: 4,
+        leavePartsOnError: false,
+        params: target,
+      })
+
+      const response = await parallelUploads3.done()
+
+      const key = response.Key
+
+      fs.rmSync(file.path)
+
+      return res.status(201).json({
+        url: `${config.AWS_CLOUDFRONT_DOMAIN}${key}`,
+      })
+    } catch (error) {
+      next(error)
     }
-    const parallelUploads3 = new Upload({
-      client: s3Client,
-      queueSize: 4,
-      leavePartsOnError: false,
-      params: target,
-    })
-
-    const response = await parallelUploads3.done()
-
-    return res.status(201).json(response)
-    // return res.status(201).json({
-    //   link: `${config.DOMAIN_NAME}data`,
-    // })
-  } catch (error) {
-    next(error)
   }
-})
-
-app.post('/data/:name', (req, res) => {
-  const name = req.params.name
-  res.redirect(config.AWS_CLOUDFRONT_DOMAIN + name)
-})
+)
 
 app.use((err, req, res, next) => {
   if (isHttpError(err) && err.statusCode < 500) {
