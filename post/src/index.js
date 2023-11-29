66dc6f89a0682638fd143b3fd4985c7ce3ba7817
Phan Thanh Tú <pttu2902@gmail.com>
2023-11-29 21:28:26 +0700

    tu dep zai

--- post/src/index.js
+++
@@ -1,7 +0,0 @@
-const express  =require('express')
-const app = express()
-const config = require('./configs/config')
-
-app.listen(config.PORT, ()=>{
-    console.log(new Date(), "listening on port:", config.PORT);
-})
