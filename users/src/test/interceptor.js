bdfcdf17fbb0d8605725c81c51043dcf9a3b4f71
tuan-hda <anhtuan.hoangdinh@gmail.com>
2023-11-30 16:34:24 +0700

    add some test cases for user service

--- users/src/test/interceptor.js
+++ users/src/test/interceptor.js
@@ -4,16 +4,27 @@ module.exports = {
     req.body = jest.fn().mockReturnValue(req)
     req.params = jest.fn().mockReturnValue(req)
     req.query = jest.fn().mockReturnValue(req)
+    req.header = (name) => {
+      return req[name]
+    }
     return req
   },
 
   mockResponse: () => {
     const res = {}
-    res.send = jest.fn().mockReturnValue(res)
-    res.status = jest.fn().mockReturnValue(res)
-    res.json = jest.fn().mockReturnValue(res)
+    res.send = jest.fn(() => res)
+    res.status = jest.fn(() => res)
+    res.json = jest.fn(() => res)
     return res
   },
 
   mockNext: () => jest.fn(),
+
+  async testExpressValidatorMiddleware(req, res, middlewares) {
+    await Promise.all(
+      middlewares.map(async (middleware) => {
+        await middleware(req, res, () => undefined)
+      })
+    )
+  },
 }
