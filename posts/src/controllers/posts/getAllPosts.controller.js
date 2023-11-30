9043565737ca28ece8a791e62c342099cd4802d4
Phan Thanh Tu <pttu2902@gmail.com>
2023-11-30 17:05:13 +0700

    update posts service

--- posts/src/controllers/posts/getAllPosts.controller.js
+++ posts/src/controllers/posts/getAllPosts.controller.js
@@ -1,11 +1,17 @@
 const PostModel = require("../../models/Post");
-const getAllPosts = async (res, next) => {
+const getAllPosts = async (req, res, next) => {
   try {
+    const page = req.query.page;
+    const limit = req.query.limit;
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
     const allPosts = await PostModel.find()
       .populate("user")
       .populate("tag")
       .exec();
-    res.status(200).json(allPosts);
+    res.status(200).json(allPosts.slice(startIndex, endIndex));
   } catch (error) {
     next(error);
   }
