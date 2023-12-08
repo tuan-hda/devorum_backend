29515b2a15667c6d1d3911bab31166f6eb62e3ab
tuan-hda <anhtuan.hoangdinh@gmail.com>
2023-12-08 15:47:11 +0700

    add update community

---
+++ community/src/controllers/updateCommunity.controller.js
@@ -0,0 +1,54 @@
+const createHttpError = require('http-errors')
+const CommunityModel = require('../models/Community')
+
+const excludeUpdateFields = new Set([
+    '_id',
+    'createdAt',
+    'updatedAt',
+    '__v',
+    'createdBy',
+    'numMembers',
+    'numPosts',
+    'name',
+])
+
+const updateCommunityController = async (req, res, next) => {
+    try {
+        const user = req.user
+        const name = req.params.name
+
+        let community = await CommunityModel.findOne({ name })
+        if (!community) {
+            throw createHttpError[404]('Community not found')
+        }
+
+        if (!user) {
+            throw createHttpError[401]('Unauthorized')
+        }
+        if (user._id != community.createdBy) {
+            throw createHttpError[403]('Forbidden')
+        }
+
+        const data = {}
+        for (const key in req.body) {
+            if (key in CommunityModel.schema.paths && !excludeUpdateFields.has(key)) {
+                data[key] = req.body[key]
+            }
+        }
+        if (Array.isArray(data.moderators) && !data.moderators.includes(user._id)) {
+            throw createHttpError[400]('Moderators must include the creator of the community')
+        }
+
+        community.set(data)
+        await community.save()
+
+        return res.status(200).json({ msg: 'Community updated' })
+    } catch (error) {
+        if (error?._message?.includes('validation failed')) {
+            return next(createHttpError[400](error.errors))
+        }
+        next(error)
+    }
+}
+
+module.exports = updateCommunityController
--- community/src/routes/community.route.js
+++ community/src/routes/community.route.js
@@ -4,18 +4,11 @@ const router = express.Router()
 const createCommunityController = require('../controllers/createCommunity.controller')
 const checkValidityCommunityName = require('../controllers/checkValidityCommunityName.controller')
 const getCommunityController = require('../controllers/getCommunity.controller')
-const { getUserProducer } = require('../broker/userProducer')
+const updateCommunityController = require('../controllers/updateCommunity.controller')
 
 router.post('/', authenticateJWT, createCommunityController)
 router.get('/validity', authenticateJWT, checkValidityCommunityName)
-router.get('/test', async (req, res, next) => {
-  try {
-    const data = await getUserProducer({ username: 'tuan-hdxa' })
-    res.status(200).json(data)
-  } catch (error) {
-    next(error)
-  }
-})
 router.get('/:name', getCommunityController)
+router.put('/:name', authenticateJWT, updateCommunityController)
 
 module.exports = router
