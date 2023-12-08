15a4c0994fbe7ae8acf8e78c64a22a62d63b28fe
tuan-hda <anhtuan.hoangdinh@gmail.com>
2023-12-08 20:59:32 +0700

    fix utils of unit test

--- users/src/test/utils.js
+++ users/src/test/utils.js
@@ -3,44 +3,44 @@ const registerUser = require('../controllers/users/registerUser.controller')
 const { mockResponse } = require('./interceptor')
 
 module.exports = {
-  initUser: () =>
-    registerUser(
-      {
-        body: {
-          username: 'test',
-          email: 'test@gmail.com',
-          password: 'abcd1234',
-        },
-      },
-      () => {},
-      () => {}
-    ),
-  initTargetUser: () =>
-    registerUser(
-      {
-        body: {
-          username: 'target',
-          email: 'target@gmail.com',
-          password: 'abcd1234',
-        },
-      },
-      () => {},
-      () => {}
-    ),
-  async loginUser(req) {
-    const res = mockResponse()
-    res.json = ({ token }) => {
-      req.Authorization = 'Bearer ' + token
-    }
-    await login(
-      {
-        body: {
-          email: 'test@gmail.com',
-          password: 'abcd1234',
-        },
-      },
-      res,
-      () => {}
-    )
-  },
+    initUser: async () =>
+        await registerUser(
+            {
+                body: {
+                    username: 'test',
+                    email: 'test@gmail.com',
+                    password: 'abcd1234',
+                },
+            },
+            () => {},
+            () => {}
+        ),
+    initTargetUser: async () =>
+        await registerUser(
+            {
+                body: {
+                    username: 'target',
+                    email: 'target@gmail.com',
+                    password: 'abcd1234',
+                },
+            },
+            () => {},
+            () => {}
+        ),
+    async loginUser(req) {
+        const res = mockResponse()
+        res.json = ({ token }) => {
+            req.Authorization = 'Bearer ' + token
+        }
+        await login(
+            {
+                body: {
+                    email: 'test@gmail.com',
+                    password: 'abcd1234',
+                },
+            },
+            res,
+            () => {}
+        )
+    },
 }
