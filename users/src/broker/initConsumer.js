7ee20379c6768bf78d600edbc42d6714ee052cff
tuan-hda <anhtuan.hoangdinh@gmail.com>
2023-12-11 17:54:20 +0700

    use local rabbitmq instead

--- users/src/broker/initConsumer.js
+++ users/src/broker/initConsumer.js
@@ -4,7 +4,7 @@ const config = require('../configs/config')
 const { getChannel } = require('./rabbitMq')
 
 const ACTIONS = {
-  getUser: getProfileService,
+    getUser: getProfileService,
 }
 
 /**
@@ -12,55 +12,71 @@ const ACTIONS = {
  * @param {amqplib.Channel} channel
  */
 const initConsumer = async () => {
-  const channel = await getChannel()
-  channel.prefetch(1)
-  await channel.assertQueue(config.RPC_QUEUE_NAME, {
-    durable: false,
-  })
+    const channel = await getChannel()
+    channel.prefetch(1)
+    await channel.assertQueue(config.RPC_QUEUE_NAME, {
+        durable: false,
+    })
 
-  channel.consume(
-    config.RPC_QUEUE_NAME,
-    async (msg) => {
-      if (!msg) {
-        return
-      }
+    channel.consume(
+        config.RPC_QUEUE_NAME,
+        async (msg) => {
+            console.log('user channel received message')
+            if (!msg) {
+                return
+            }
 
-      // Init response data
-      let responseData = null
+            // Init response data
+            let responseData = null
 
-      try {
-        // Parse payload from message
-        const payload = JSON.parse(msg.content.toString())
-        const { action, data } = payload
+            console.log('processing message')
+            try {
+                // Parse payload from message
+                const payload = JSON.parse(msg.content.toString())
+                console.log('message payload', payload)
+                const { action, data } = payload
 
-        // Check if action is valid
-        if (!(action in ACTIONS)) {
-          return
-        }
+                // Check if action is valid
+                if (!(action in ACTIONS)) {
+                    console.log('action not found', action)
+                    return
+                }
 
-        // Perform some operation based on action
-        const result = await ACTIONS[action](data)
-        // Set response data if success
-        responseData = {
-          data: result,
-          error: null,
-        }
-      } catch (error) {
-        // Set error if failed
-        responseData = {
-          data: null,
-          error: String(error),
-        }
-      } finally {
-        // Send and ack message
-        channel.sendToQueue(msg.properties.replyTo, Buffer.from(JSON.stringify(responseData)), {
-          correlationId: msg.properties.correlationId,
-        })
-        channel.ack(msg)
-      }
-    },
-    { noAck: false }
-  )
+                // Perform some operation based on action
+                console.log('perform action', action)
+                console.time('perform-action')
+                const result = await ACTIONS[action](data)
+                console.timeEnd('perform-action')
+
+                // Set response data if success
+                responseData = {
+                    data: result,
+                    error: null,
+                }
+                console.log('response data', responseData)
+            } catch (error) {
+                // Set error if failed
+                responseData = {
+                    data: null,
+                    error: String(error),
+                }
+                console.log('failed when performing action', responseData)
+            } finally {
+                // Send and ack message
+                console.log('send response data')
+                channel.sendToQueue(
+                    msg.properties.replyTo,
+                    Buffer.from(JSON.stringify(responseData)),
+                    {
+                        correlationId: msg.properties.correlationId,
+                    }
+                )
+                channel.ack(msg)
+                console.log('end processing message')
+            }
+        },
+        { noAck: false }
+    )
 }
 
 module.exports = initConsumer
