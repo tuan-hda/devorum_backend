eb1968142c6c770822fcca540641aca997d02aaf
Phan Thanh Tu <pttu2902@gmail.com>
2023-12-12 17:53:38 +0700

    complete login getPosts with pagination

--- posts/src/routes/posts.route.js
+++ posts/src/routes/posts.route.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const getAllPosts = require("../controllers/posts/getAllPosts.controller");
+const paginated = require("../middlewares/posts.middleware");
+const getPosts = require("../controllers/posts/getPosts.controller");
 const { getUserProducer } = require("../broker/userProducer");
 
-router.get("/", getAllPosts);
+router.get("/", paginated, getPosts);
 router.get("/test", async (req, res, next) => {
   try {
-    const data = await getUserProducer({ id: ["657739e9a97eb9bea8d7b7a6"] });
+    const data = await getUserProducer({ id: ["656e96252d8e5d48d0c8cd7e"] });
     res.status(200).json(data[0]);
   } catch (error) {
     next("error when test", error);
