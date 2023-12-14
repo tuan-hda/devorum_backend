b52b4606726d048d5c49d2371fdf7939c4f020b0
tuan-hda <anhtuan.hoangdinh@gmail.com>
2023-12-14 21:01:22 +0700

    add judging system

--- chat/src/middlewares/auth.middleware.js
+++ chat/src/middlewares/auth.middleware.js
@@ -3,28 +3,28 @@ const config = require('../configs/config')
 const createHttpError = require('http-errors')
 
 const authenticateJWT = (req, res, next, allowFail = false) => {
-  const authorization = req.header('Authorization')
+    const authorization = req.header('Authorization')
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    if (allowFail) {
-      return next()
-    } else {
-      throw createHttpError(401)('No bearer token provided')
-    }
-  }
-
-  const accessToken = authorization.slice(7)
-
-  jwt.verify(accessToken, config.JWT_SECRET, (err, user) => {
-    if (err) {
-      throw createHttpError[403]('Invalid token')
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+        if (allowFail) {
+            return next()
+        } else {
+            throw createHttpError[401]('No bearer token provided')
+        }
     }
 
-    req.user = user
-    next()
-  })
+    const accessToken = authorization.slice(7)
+
+    jwt.verify(accessToken, config.JWT_SECRET, (err, user) => {
+        if (err) {
+            throw createHttpError[403]('Invalid token')
+        }
+
+        req.user = user
+        next()
+    })
 }
 
 module.exports = {
-  authenticateJWT,
+    authenticateJWT,
 }
