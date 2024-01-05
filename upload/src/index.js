902298d4c2fdaf6c2d74a24b1b01adb840b5dec7
Phan Thanh Tú <pttu2902@gmail.com>
2024-01-05 21:54:34 +0700

    fix: change console log message BREAKING CHANGE: change from "listening on port" to "Listening on port"

--- upload/src/index.js
+++ upload/src/index.js
@@ -1,93 +1,96 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const config = require('./configs/config.js')
-const bodyParser = require('body-parser')
-const { isHttpError } = require('http-errors')
-const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
-const { Upload } = require('@aws-sdk/lib-storage')
-const { S3Client } = require('@aws-sdk/client-s3')
-const fs = require('fs')
-const createHttpError = require('http-errors')
-const { v4: uuidv4 } = require('uuid')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const config = require("./configs/config.js");
+const bodyParser = require("body-parser");
+const { isHttpError } = require("http-errors");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const { Upload } = require("@aws-sdk/lib-storage");
+const { S3Client } = require("@aws-sdk/client-s3");
+const fs = require("fs");
+const createHttpError = require("http-errors");
+const { v4: uuidv4 } = require("uuid");
 
-app.use(cors({ origin: config.whitelist, credentials: true }))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(cors({ origin: config.whitelist, credentials: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const s3Client = new S3Client({
   region: config.AWS_BUCKET_REGION,
-  credentials: { accessKeyId: config.AWS_ACCESS_KEY, secretAccessKey: config.AWS_SECRET_KEY },
-})
+  credentials: {
+    accessKeyId: config.AWS_ACCESS_KEY,
+    secretAccessKey: config.AWS_SECRET_KEY,
+  },
+});
 
 app.post(
-  '/',
+  "/",
   (_, __, next) => {
     try {
-      fs.mkdirSync('uploads')
+      fs.mkdirSync("uploads");
     } catch (error) {
-      if (error.code !== 'EEXIST') {
-        console.log(error)
+      if (error.code !== "EEXIST") {
+        console.log(error);
       }
     }
-    next()
+    next();
   },
-  upload.single('file'),
+  upload.single("file"),
   async (req, res, next) => {
     try {
       if (req.file == null) {
-        return createHttpError[400]('You must choose a file')
+        return createHttpError[400]("You must choose a file");
       }
-      var file = req.file
+      var file = req.file;
 
       const target = {
         Bucket: config.AWS_BUCKET_NAME,
         region: config.DOMAIN_NAME,
-        Key: uuidv4() + '-' + req.file.originalname,
+        Key: uuidv4() + "-" + req.file.originalname,
         Body: fs.createReadStream(file.path),
-      }
+      };
       const parallelUploads3 = new Upload({
         client: s3Client,
         queueSize: 4,
         leavePartsOnError: false,
         params: target,
-      })
+      });
 
-      const response = await parallelUploads3.done()
+      const response = await parallelUploads3.done();
 
-      const key = response.Key
+      const key = response.Key;
 
-      fs.rmSync(file.path)
+      fs.rmSync(file.path);
 
       return res.status(201).json({
         url: `${config.AWS_CLOUDFRONT_DOMAIN}${key}`,
-      })
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
-)
+);
 
 app.use((err, req, res, next) => {
   if (isHttpError(err) && err.statusCode < 500) {
     return res.status(err.statusCode).json({
       msg: err.message,
-    })
+    });
   }
 
-  if (err.kind === 'ObjectId') {
+  if (err.kind === "ObjectId") {
     return res.status(400).json({
-      msg: 'Invalid id',
-    })
+      msg: "Invalid id",
+    });
   }
 
-  console.log(err)
+  console.log(err);
   res.status(500).json({
-    msg: 'Internal server error',
-  })
-})
+    msg: "Internal server error",
+  });
+});
 
 app.listen(config.PORT, () => {
-  console.log(new Date(), 'listening on port:', config.PORT)
-})
+  console.log(new Date(), "Listening on port:", config.PORT);
+});
